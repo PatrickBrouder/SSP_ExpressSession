488f15cf867a5e1a366e06026f957e3299c082bb
PatrickBrouder <K00203617@student.lit.ie>
2017-02-27 19:13:39 +0000

    5

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var myJokes = [];
-/* GET home page. */
+var myJokes =  req.session.myJokes ||  [];
+
 router.get('/', function(req, res, next) {
   
   res.render('index', { title: 'Joke session' });
 });
 router.post('/jokes', function(req, res) {
-  //var joke1=  req.body.joke
   req.session.jokeMsg = req.body.joke;
-  //var myjoke= req.session.jokeMsg;
   myJokes.push(req.session.jokeMsg);
   res.render('jokes',{ jokeList: myJokes });
 });
